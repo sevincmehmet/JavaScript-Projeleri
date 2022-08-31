4ed2e041cc7d6ce467c89514cfdbf2e42d9fbde1
Mehmet Ali SEVINC <kemalist611661@gmail.com>
2022-08-31 18:48:33 +0300

    fazlalık kod bloğu silindi

--- TEXT_VOİCE_OVER/js/script.js
+++ TEXT_VOİCE_OVER/js/script.js
@@ -4,9 +4,6 @@ let btn = document.querySelector('.submit')
 
 btn.addEventListener("click", speak)
 
-var message = $('#myText').val();
-
-
 function speak () {
     responsiveVoice.speak(textArea.value, "US English Male")
 }
